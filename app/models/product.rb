7e15ac762285c77dca44f126d85be7ed93d5f104
Maria Ameen <Ameen.maria.s@gmail.com>
2022-12-14 13:13:12 -0500

    code for potential review-related stuff

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,21 @@
 class Product < ApplicationRecord
 end
+
+
+# Might want these features later:
+# has_many :product_categories
+# has_many :categories, through: :product_categories
+# has_many :reviews
+# has_many :users, through: :reviews
+
+# def self.search(search)
+#   if search
+#     where("name LIKE ?", "%#{search}%")
+#   else
+#     all
+#   end
+# end
+
+# def average_rating
+#   reviews.average(:rating).to_f
+# end
