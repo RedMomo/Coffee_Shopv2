c5841d1be21d8692f7782102c7cc69530c3aee02
Maria Ameen <Ameen.maria.s@gmail.com>
2022-12-27 21:16:17 -0500

    security push

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,24 @@
 class ProductsController < ApplicationController
+    def index
+        @products = Product.all
+        render json: @products
+    end
+
+    def show
+        @product = Product.find(params[:id])
+        render json: @product
+    end
+
+    # def create
+    #     @product = Product.new(product_params)
+    #     if @product.save
+    #         render json: @product
+    #     else
+    #         render json: @product.errors, status: :unprocessable_entity
+    #     end
+    # end
 end
+
+# Questions for Michael:
+# 1. why does autopilot do the instance instead of just products
+# 2. why product.new and .save instead of .create
